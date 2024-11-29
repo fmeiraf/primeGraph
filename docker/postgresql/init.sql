bab0a41e89bf39788cbf87f9bf366680cfdad77f
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2024-11-28 22:16:26 -0700

    added best practices on postgresql stuff

--- docker/postgresql/init.sql
+++ docker/postgresql/init.sql
@@ -1,15 +1,15 @@
 -- Create the checkpoints table
 CREATE TABLE IF NOT EXISTS checkpoints (
-    checkpoint_id VARCHAR(255) PRIMARY KEY,
-    chain_id VARCHAR(255) NOT NULL,
-    chain_status VARCHAR(50) NOT NULL,
-    state_class VARCHAR(255) NOT NULL,
-    state_version VARCHAR(50),
+    checkpoint_id VARCHAR PRIMARY KEY,
+    chain_id VARCHAR NOT NULL,
+    chain_status VARCHAR NOT NULL,
+    state_class VARCHAR NOT NULL,
+    state_version VARCHAR,
     data JSONB NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    next_execution_node VARCHAR(255),
+    timestamp TIMESTAMPTZ NOT NULL,
+    next_execution_node VARCHAR,
     executed_nodes JSONB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Create indexes
